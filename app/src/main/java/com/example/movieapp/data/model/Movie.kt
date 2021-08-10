6da13375dd2a2a939600ac8e5e58c64993ed222d
Juan Alcaraz <juan5alcaraz@gmail.com>
2021-08-09 23:25:25 -0500

    Entety y Dao creados

--- app/src/main/java/com/example/movieapp/data/model/Movie.kt
+++ app/src/main/java/com/example/movieapp/data/model/Movie.kt
@@ -1,6 +1,9 @@
 package com.example.movieapp.data.model
 
 import android.text.BoringLayout
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 data class Movie(
     val id: Int = -1,
@@ -20,3 +23,33 @@ data class Movie(
 )
 
 data class MovieList(val results:List<Movie> = listOf())
+
+@Entity
+data class MovieEntity(
+    @PrimaryKey
+    val id: Int = -1,
+    @ColumnInfo(name="adult")
+    val adult: Boolean = false,
+    @ColumnInfo(name="backdrop_path")
+    val backdrop_path: String = "",
+    @ColumnInfo(name="original_language")
+    val original_language: String = "",
+    @ColumnInfo(name="original_title")
+    val original_title: String = "",
+    @ColumnInfo(name="overview")
+    val overview: String = "",
+    @ColumnInfo(name="popularity")
+    val popularity: Double = 0.0,
+    @ColumnInfo(name="poster_path")
+    val poster_path:String="",
+    @ColumnInfo(name="release_date")
+    val release_date: String = "",
+    @ColumnInfo(name="title")
+    val title:String="",
+    @ColumnInfo(name="video")
+    val video:Boolean=false,
+    @ColumnInfo(name="vote_average")
+    val vote_average: Double = 0.0,
+    @ColumnInfo(name="vote_account")
+    val vote_account: Int = -1
+)
